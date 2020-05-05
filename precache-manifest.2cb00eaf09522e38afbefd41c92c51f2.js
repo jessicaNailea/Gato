7fd9dfe1234a1d67c713d300c39a360fcd18bbb8
jessicaBuenfil <jessibuenfilsolis@gmail.com>
2020-05-05 16:10:16 -0500

    Updates

---
+++ precache-manifest.2cb00eaf09522e38afbefd41c92c51f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7238b17a77e9640477df44bb1f32cbf8",
+    "url": "/gato/index.html"
+  },
+  {
+    "revision": "8fd0f24e776f132bb67e",
+    "url": "/gato/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "3d00a59e5ff6934b9b6b",
+    "url": "/gato/static/js/2.0d01db58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gato/static/js/2.0d01db58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fd0f24e776f132bb67e",
+    "url": "/gato/static/js/main.b74c493e.chunk.js"
+  },
+  {
+    "revision": "d8e72d0acf7be1b6927a",
+    "url": "/gato/static/js/runtime-main.02101773.js"
+  }
+]);
